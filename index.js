5ce8c52ef31a07ca1a100df8754547be111808ad
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-04-15 11:52:36 +0200

    Create index.js

---
+++ index.js
@@ -0,0 +1,39 @@
+const wapiFetch = (url, options = {}) => {
+        
+      const requestId = crypto.randomUUID();
+      
+      return new Promise((resolve, reject) => {
+          const handleMessage = (event) => {
+          if (event.data.type === 'WAPI_RESPONSE' && event.data.id === requestId) {
+              
+              window.removeEventListener('message', handleMessage);
+              
+              if (event.data.response.success) {
+              
+              const fakeResponse = new Response(
+                  event.data.response.data.body,
+                  {
+                  status: event.data.response.data.status,
+                  headers: event.data.response.data.headers
+                  }
+              );
+              resolve(fakeResponse);
+              } else {
+                reject(new Error(event.data.response.error));
+              }
+          }
+          };
+      
+          window.addEventListener('message', handleMessage);
+          
+          window.postMessage({
+          type: 'WAPI_PROXY',
+          id: requestId,
+          url: url,
+          options: options
+          }, '*');
+      });
+  
+};
+
+export { wapiFetch };
