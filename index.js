756ad2645d19b4c5c70251a0d7c5559c67dd03f6
Lorenzo Gobbo <106741440+logo94@users.noreply.github.com>
2025-04-15 12:20:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -3,34 +3,35 @@ const wapiFetch = (url, options = {}) => {
       const requestId = crypto.randomUUID();
       
       return new Promise((resolve, reject) => {
+              
           const handleMessage = (event) => {
-          if (event.data.type === 'WAPI_RESPONSE' && event.data.id === requestId) {
-              
-              window.removeEventListener('message', handleMessage);
-              
-              if (event.data.response.success) {
-              
-              const fakeResponse = new Response(
-                  event.data.response.data.body,
-                  {
-                  status: event.data.response.data.status,
-                  headers: event.data.response.data.headers
+                  
+                  if (event.data.type === 'WAPI_RESPONSE' && event.data.id === requestId) {
+                      
+                      window.removeEventListener('message', handleMessage);
+                      
+                      if (event.data.response.success) {
+                      
+                              const fakeResponse = new Response(
+                                  event.data.response.data.body,
+                                  {
+                                  status: event.data.response.data.status,
+                                  headers: event.data.response.data.headers
+                                  }
+                              );
+                              
+                              resolve(fakeResponse);
+                      }
                   }
-              );
-              resolve(fakeResponse);
-              } else {
-                reject(new Error(event.data.response.error));
-              }
-          }
           };
       
           window.addEventListener('message', handleMessage);
           
           window.postMessage({
-          type: 'WAPI_PROXY',
-          id: requestId,
-          url: url,
-          options: options
+                  type: 'WAPI_PROXY',
+                  id: requestId,
+                  url: url,
+                  options: options
           }, '*');
       });
   
